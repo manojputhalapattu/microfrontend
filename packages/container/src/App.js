e846defdb2cf0c7bea2bf2d9c887e9376232ca78
puthalapattumanoj <puthalapattu.manoj@puthalapattumanojs-MacBook-Pro.local>
2022-09-14 21:09:06 +0530

    intial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+import MarketingApp from './components/MarketingApp';
+
+
+
+console.log(mount);
+
+export default () => { 
+    return (
+        <div> 
+            <h1> Hi there in container </h1>
+            <hr/> 
+            <MarketingApp />
+        </div>
+    );
+};
---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,21 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+  
+
+
+// which means babels is going to preset the react and other imports
