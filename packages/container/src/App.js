cc4c21c4b02050f60476911a6f70864091b14fc5
puthalapattumanoj <puthalapattu.manoj@puthalapattumanojs-MacBook-Pro.local>
2022-10-07 21:21:07 +0530

    feat(all): Added header file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
-
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 
 console.log(mount);
 
 export default () => { 
     return (
+        <BrowserRouter> 
         <div> 
-            <h1> Hi there in container !!! </h1>
-            <hr/> 
+            <Header/>
             <MarketingApp />
         </div>
+         </BrowserRouter>
+        
     );
 };
