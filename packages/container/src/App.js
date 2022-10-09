cc9c6f48a6c658da11388338bc6b6307c4299442
puthalapattumanoj <puthalapattu.manoj@puthalapattumanojs-MacBook-Pro.local>
2022-10-09 12:49:30 +0530

    feat(all): fix header styles file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,17 +3,22 @@ import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-
-console.log(mount);
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+})
 
 export default () => { 
     return (
         <BrowserRouter> 
+        <StylesProvider generateClassName={generateClassName}>
         <div> 
             <Header/>
             <MarketingApp />
         </div>
+             </StylesProvider>
+        
          </BrowserRouter>
         
     );
