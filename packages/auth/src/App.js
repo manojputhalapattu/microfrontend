3f837bb095743a45fe6bc7702ba3b554de4b268a
puthalapattumanoj <puthalapattu.manoj@puthalapattumanojs-MacBook-Pro.local>
2022-10-23 12:59:22 +0530

    feat(all): remove build

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Router, UseState } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
